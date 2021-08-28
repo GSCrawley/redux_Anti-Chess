30c6c40082c6b1ececac2f78a4a2f24bfaba57fe
Gideon Crawley <gscrawley@gmail.com>
2021-08-28 12:37:35 -0700

    MovesSystem component complete

---
+++ src/components/MovesSystem.js
@@ -0,0 +1,23 @@
+import store from '@redux/store';
+import { buildDefaultEntities } from '@utilities/entityUtility';
+import { squareClicked } from '@redux/actions/squares-actions';
+import { getSquareSelected, inBounds } from '@utilities/squareUtility';
+
+const MoveSystem = (entities, { input }) => {
+    const { game, squares } = store.getState();
+    const result = game.gameWasReset ? buildDefaultEntities(game, squares) : { ...entities };
+
+    const offset = 0;
+    const xMax = 800;
+    const yMax = 800;
+    const { payload } = input.find(x => x.name === 'onMouseDown' || {};
+
+    if (inBounds({ location: payload, offset, xMax, yMax })) {
+        const { row, col } = getSquareSelected(payload);
+        store.dispatch(squareClicked({ game, squares, row, col }));
+    }
+    return result;
+
+};
+
+export default MovesSystem;
