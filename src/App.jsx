fceb075d4ab46d8b9663dd79a02bd4e859e5be64
Gideon Crawley <gscrawley@gmail.com>
2021-08-28 11:57:37 -0700

    lets go

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '@babel/polyfill';
+import '@styles/css/main.css';
+import { Provider } from 'react-redux';
+import store from '@redux/store';
+import AntiChess from '@pages/AntiChess.jsx';
+
+
+const App = () => (
+  <>
+    <Provider store={store}>
+      <AntiChess />
+    </Provider>
+  </>
+);
+
+export default App;
